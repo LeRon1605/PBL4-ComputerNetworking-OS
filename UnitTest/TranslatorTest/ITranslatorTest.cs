59dd0aee6abc7af1b089365ae998b537e9ac45f8
Ron Le <ronle9519@gmail.com>
2022-09-17 20:42:22 +0700

    vietnamese translator test

---
+++ UnitTest/TranslatorTest/ITranslatorTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTest.TranslatorTest
+{
+    public interface ITranslatorTest
+    {
+        void GivenSimpleNumber_WhenTranslate_ThenShouldWork(string number, string expected);
+        void GivenLargeNumber_WhenTranslate_ThenShouldWork(string number, string expected);
+        void GivenInvalidNumber_WhenTranslate_ThenShouldWork(string number);
+        void GivenLeadZeroNumber_WhenTranslate_ThenShouldWork(string number, string expected);
+        void GivenTailZeroNumber_WhenTranslate_ThenShouldWork(string number, string expected);
+    }
+}
