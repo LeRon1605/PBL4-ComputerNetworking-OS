53328906ad79dee81d758fb9ef2e223ce76d00ed
= <=>
2022-10-03 12:06:53 +0700

    English

--- ServerApp/Translator/EnglishTranslator.cs
+++ ServerApp/Translator/EnglishTranslator.cs
@@ -1,14 +1,103 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace ServerApp.Translator
 {
     public class EnglishTranslator : ITranslator
     {
+        private static readonly string[] digits = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight",
+        "nine", "ten","eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+
+        private static readonly string[] units = { "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};  
+        
+        private static readonly string[] denom = { "", "thousand", "million", "billion", "trillion", "quadrillion", "quintillion", 
+        "sextillion", "septillion", "octillion", "nonillion", "decillion", "undecillion", "duodecillion", "tredecillion", 
+        "quattuordecillion", "sexdecillion", "septendecillion", "octodecillion", "novemdecillion", "vigintillion" };
+        
+        public string convert_xx(string number)
+        {
+            double _number = Convert.ToDouble(number);
+            if (_number < 20)
+            {
+                return digits[Convert.ToInt32(_number)];    
+            }
+            for (int i = 0; i < units.Length; i++)
+            {
+                string unit = units[i];
+                double value = 20 + 10 * i;
+                if (value + 10 > _number)
+                {
+                    if ((_number % 10) != 0)
+                    {
+                        return unit + "-" + digits[Convert.ToInt32(_number) % 10];
+                    }
+                    return unit;
+                }
+            }
+            throw new Exception();
+        }
+        public string convert_xxx(string number)
+        {
+            double _number = Convert.ToSingle(number);
+            string result = "";
+            int rem = Convert.ToInt32(Math.Floor(_number / 100));
+            int mod = Convert.ToInt32(_number % 100);
+            if (rem > 0)
+            {
+                result = digits[rem] + " hundred";
+                if (mod > 0)
+                {
+                    result = result + " ";
+                }
+            }
+            if (mod > 0)
+            {
+                result = result + convert_xx(mod.ToString());
+            }
+            return result;
+        }
         public string Translate(string number)
         {
-            return null;
+            string result = "";
+
+            if (number[0] == '-')
+            {
+                number = number.Substring(1);
+                return "Minus " + Translate(number);
+            }
+
+            double _number = Convert.ToDouble(number);
+
+            if (_number < 100)
+            {
+                result = convert_xx(number);
+                return char.ToUpper(result[0]) + result.Substring(1);
+            }
+            if (_number < 1000)
+            {
+                result = convert_xxx(number);
+                return char.ToUpper(result[0]) + result.Substring(1);
+            }
+            for (int v = 0; v < denom.Length; v++)
+            {
+                int didx = v - 1;
+                double dval = Convert.ToDouble(Math.Pow(1000, v));
+                if (dval > _number)
+                {
+                    double mod = (double)(Math.Pow(1000, didx));
+                    int l = Convert.ToInt32(Math.Floor(_number / mod));
+                    double r = _number - (l * mod);
+                    result = convert_xxx(l.ToString()) + " " + denom[didx];
+                    if (r > 0)
+                    {
+                        result = result + ", " + Translate(r.ToString());
+                    }
+                    return char.ToUpper(result[0]) + result.Substring(1);
+                }
+            }
+            throw new Exception();
         }
     }
 }
---
+++ UnitTest/TranslatorTest/EnglishTranslatorTest.cs
@@ -0,0 +1,80 @@
+﻿using ServerApp.Translator;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace UnitTest.TranslatorTest
+{
+    public class EnglishTranslatorTest : ITranslatorTest
+    {
+        private readonly ITranslator translator;
+        public EnglishTranslatorTest()
+        {
+            translator = new EnglishTranslator();
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("1a3")]
+        [InlineData("_12@3")]
+        public void GivenInvalidNumber_WhenTranslate_ThenShouldReturnNull(string number)
+        {
+            string actual = translator.Translate(number);
+
+            Assert.Null(actual);
+        }
+
+        [Theory]
+        [InlineData("1234", "One thousand two hundred and thirty four")]
+        [InlineData("1200200", "One million two hundred thousand and two hundred")]
+        [InlineData("1234567891", "One billion two hundred thirty four million five hundred sixty seven thousand and eight hundred ninety one")]
+        [InlineData("1200000", "One million and two hundred thousand")]
+        [InlineData("1000002", "One million and two")]
+        public void GivenLargeNumber_WhenTranslate_ThenShouldWork(string number, string expected)
+        {
+            string actual = translator.Translate(number);
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("0", "Zero")]
+        [InlineData("01", "One")]
+        [InlineData("010", "Ten")]
+        [InlineData("0000123456", "One hundred twenty three thousand and four hundred fifty six")]
+        public void GivenLeadZeroNumber_WhenTranslate_ThenShouldWork(string number, string expected)
+        {
+            string actual = translator.Translate(number);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("1", "One")]
+        [InlineData("12", "Twelve")]
+        [InlineData("10", "Ten")]
+        [InlineData("11", "Eleven")]
+        [InlineData("15", "fifteen")]
+        [InlineData("125", "One hundred and twenty five")]
+        [InlineData("101", "One hundred and one")]
+        public void GivenSimpleNumber_WhenTranslate_ThenShouldWork(string number, string expected)
+        {
+            string actual = translator.Translate(number);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("10", "Ten")]
+        [InlineData("100", "One hundred")]
+        [InlineData("1000", "One thousand")]
+        [InlineData("1000000", "One million")]
+        [InlineData("1000000000", "One billion")]
+        public void GivenTailZeroNumber_WhenTranslate_ThenShouldWork(string number, string expected)
+        {
+            string actual = translator.Translate(number);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
