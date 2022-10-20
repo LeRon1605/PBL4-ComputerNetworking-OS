2a639c401c5ca9a321bc3639edcdbc8ffa62fe6e
= <=>
2022-10-20 20:38:22 +0700

    Translator English + France

--- ServerApp/Translator/FrenchTranslator.cs
+++ ServerApp/Translator/FrenchTranslator.cs
@@ -1,52 +1,92 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.NetworkInformation;
 using System.Text;
 
 namespace ServerApp.Translator
 {
     public class FrenchTranslator : ITranslator
     {
-        private static readonly string[] digits = { "zéro", "un", "deux", "trois", "quatre", "cinq", "six", "sept", "huit",
-        "neuf", "dix","onze ", "douze", "treize", "quatorze", "quinze", "seize", "dix-sept", "dix-huit", "dix-neuf"};
+        private static readonly string[] digits = {"zéro", "un", "deux", "trois", "quatre", "cinq", "six", "sept", "huit",
+        "neuf", "dix","onze", "douze", "treize", "quatorze", "quinze", "seize", "dix-sept", "dix-huit", "dix-neuf"};
 
-        private static readonly string[] dozens = { "vingt", "trente", "quarante", "cinquante", "soixante", "soixante-dix", "quatre-vingts", "quatre-vingt-dix" };
+        private static readonly string[] dozens = {"vingt", "trente", "quarante", "cinquante", "soixante", "soixante-dix", "quatre-vingt", "quatre-vingt-dix" };
 
-        private static readonly string[] denom = { "", "" , "mille", "million", "milliard", "billion", "billiard", "trillion",
+        private static readonly string[] units = {"", "mille", "million", "milliard", "billion", "billiard", "trillion",
         "trilliard", "quadrillion", "quadrilliard", "quintillion", "quintilliard", "sextillion", "sextilliard", "septillion",
         "septilliard", "octillion", "octilliard", "nonillion", "nonilliard", "décillion", "décilliard", "undecillion", "undecilliard",
         "duodecillion", "duodecilliard", "tredecillion", "tredecilliard", "quattuordecillion", "quattuordecilliard", "quindecillion", "quindecilliard",
         "sexdecillion", "sexdecilliard", "septdecillion", "septdecilliard"};
+
         public string convert_xx(string number)
         {
-            double _number = Convert.ToDouble(number);
+            int _number = Convert.ToInt32(number);
             if (_number < 20)
             {
-                return digits[Convert.ToInt32(_number)];
+                return digits[_number];
             }
             for (int i = 0; i < dozens.Length; i++)
             {
-                string dezon = dozens[i];
-                double value = 20 + 10 * i;
+                string dozen = dozens[i];
+                int value = 20 + 10 * i;
                 if (value + 10 > _number)
                 {
                     if ((_number % 10) != 0)
                     {
-                        return dezon + "-" + digits[Convert.ToInt32(_number) % 10];
+                        if(_number >= 70 && _number < 100)
+                        {
+                            if(_number < 80)
+                            {
+                                if (_number % 10 == 1)
+                                {
+                                    return dozen + " et " + digits[(_number % 10) + 10];
+                                }
+                                else
+                                {
+                                    return dozen + "-" + digits[(_number % 10) + 10];
+                                }
+                            }
+                            else if (_number < 90)
+                            {
+                                return dozen + "-" + digits[_number % 10];
+                            }
+                            else
+                            {
+                                dozen = dozens[i - 1];
+                                return dozen + "-" + digits[(_number % 10) + 10];
+                            }
+
+                        }
+                        if(_number % 10 == 1)
+                        {
+                            return dozen + " et " + digits[_number % 10];
+                        }
+                        else
+                        {
+                            return dozen + "-" + digits[_number % 10];
+                        }   
                     }
-                    return dezon;
+                    return dozen;
                 }
             }
             return "";
         }
         public string convert_xxx(string number)
         {
-            double _number = Convert.ToSingle(number);
+            int _number = Convert.ToInt32(number);
             string result = "";
-            int rem = Convert.ToInt32(Math.Floor(_number / 100));
-            int mod = Convert.ToInt32(_number % 100);
+            int rem = _number / 100;
+            int mod = _number % 100;
             if (rem > 0)
             {
-                result = digits[rem] + " cent";
+                if(rem == 1)
+                {
+                    result = "cent";
+                }
+                else
+                {
+                    result = digits[rem] + " cent";
+                }
                 if (mod > 0)
                 {
                     result = result + " ";
@@ -59,12 +99,12 @@ namespace ServerApp.Translator
             return result;
         }
 
-        public string TranslateNumber(string number)
+        public string HandlerNumber(string number)
         {
 
             string result = "";
 
-            double _number = Convert.ToDouble(number);
+            int _number = Convert.ToInt32(number);
 
             if (_number < 100)
             {
@@ -75,22 +115,6 @@ namespace ServerApp.Translator
             {
                 result = convert_xxx(number);
                 return char.ToUpper(result[0]) + result.Substring(1);
-            }
-            for (int v = 0; v < denom.Length; v++)
-            {
-                double dval = Convert.ToDouble(Math.Pow(1000, v));
-                if (dval > _number)
-                {
-                    double mod = (double)(Math.Pow(1000, v - 1));
-                    int l = Convert.ToInt32(Math.Floor(_number / mod));
-                    double r = _number - (l * mod);
-                    result = convert_xxx(l.ToString()) + " " + denom[v];
-                    if (r > 0)
-                    {
-                        result = result + ", " + TranslateNumber(r.ToString());
-                    }
-                    return char.ToUpper(result[0]) + result.Substring(1);
-                }
             }
             return "";
         }
@@ -107,11 +131,11 @@ namespace ServerApp.Translator
 
             number = number.TrimStart('0').PadLeft(1, '0');
 
-
+            bool isNegative = false;
             if (number[0] == '-')
             {
                 number = number.Substring(1);
-                return "Moins " + Translate(number);
+                isNegative = true;
             }
 
             List<string> numberSplit = new List<string>();
@@ -126,26 +150,73 @@ namespace ServerApp.Translator
                 if (count % 3 == 0 || i == 0)
                 {
                     string subNumber = number.Substring(i, count);
-                    for (int j = 0; j < numberSplit.Count; j++)
-                    {
-                        subNumber += "000";
-                    }
                     numberSplit.Add(subNumber);
                     count = 0;
                 }
+            }
+
+            // Negative number
+            if (isNegative)
+            {
+                return "Minos " + Translate(number);
             }
 
             string result = "";
 
             for (int i = numberSplit.Count - 1; i >= 0; i--)
             {
-                if (i != 0)
+                if (i == 1)
                 {
-                    result += TranslateNumber(numberSplit[i]) + ", ";
+                    if (HandlerNumber(numberSplit[i]) == "Zéro")
+                    {
+                        result += "";
+                    }
+                    else
+                    {
+
+                    }
+                    {
+                        if(HandlerNumber(numberSplit[i]) == "Un")
+                        {
+                            result += units[i] + ", ";
+
+                        }
+                        else
+                        {
+                            result += HandlerNumber(numberSplit[i]) + " " + units[i] + ", ";
+                        }
+                    }
+                }
+                else if (i != 0 && i != 1)
+                {
+                    if (HandlerNumber(numberSplit[i]) == "Zéro")
+                    {
+                        result += "";
+                    }
+                    else
+                    {
+                        if (HandlerNumber(numberSplit[i]) == "Un")
+                        {
+                            result += HandlerNumber(numberSplit[i]) + " " + units[i] + ", ";
+
+
+                        }
+                        else
+                        {
+                            result += HandlerNumber(numberSplit[i]) + " " + units[i] + "s, ";
+                        }
+                    }
                 }
                 else
                 {
-                    result += TranslateNumber(numberSplit[i]);
+                    if (HandlerNumber(numberSplit[i]) == "Zéro")
+                    {
+                        result += "";
+                    }
+                    else
+                    {
+                        result += HandlerNumber(numberSplit[i]);
+                    }
                 }
             }
             return char.ToUpper(result[0]) + result.Substring(1);
